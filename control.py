308e73e6379e676b8198e156650f94339fc10b2a
amarschn <andrew.marschner@gmail.com>
2020-02-21 16:53:04 -0600

    Working w/NEMA17 motors

--- control.py
+++ control.py
@@ -1,52 +1,71 @@
 #!/usr/bin/env python
-"""\
+"""
 Simple g-code streaming script for grbl
 """
 
 import serial
 import time
 
-FIRST_DRIVE_AXIS = 'y'
-SECOND_DRIVE_AXIS = 'z'
+NUDGER_AXIS = 'y'
+TAKEAWAY_AXIS = 'z'
+ELEVATOR_AXIS = 'x'
+
+NUDGER_FEED_DIR = 1
+TAKEAWAY_FEED_DIR = 1
+ELEVATOR_FEED_DIR = 1
+
 
 def initialize():
-	s = serial.Serial('/dev/cu.usbmodem14201',115200)
-	# Wake up grbl
-	s.write("\r\n\r\n")
-	time.sleep(2)   # Wait for grbl to initialize
-	execute_command(s, 'g92y0z0')
-	return s
+    """
+    """
+    s = serial.Serial('/dev/cu.usbmodem14201',115200)
+    # Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize
+    execute_command(s, 'g92x0y0z0')
+    return s
 
 
 def execute_command(s, command):
-	s.flushInput()  # Flush startup text in serial input
-	s.write('{}\n'.format(command.strip()))
-	grbl_out = s.readline()
-	print 'grbl : ' + grbl_out.strip()
+    """
+    """
+    s.flushInput()  # Flush startup text in serial input
+    s.write('{}\n'.format(command.strip()))
+    grbl_out = s.readline()
+    print 'grbl : ' + grbl_out.strip()
 
 
-def move(s, first_drive_distance=0, second_drive_distance=0, speed=180):
-	"""
-	Movement in millimeters of the first drive (nudger + retard rolls)
-	"""
-	d1 = -first_drive_distance
-	d2 = -second_drive_distance
-	execute_command(s, 'f{}'.format(speed))
-	execute_command(s, 'g92y0z0')
-	execute_command(s, 'g1{}{}{}{}'.format(FIRST_DRIVE_AXIS,
-										   d1,
-										   SECOND_DRIVE_AXIS,
-										   d2))
+def move(s, nudger_distance=0, takeaway_distance=0, elevator_distance=0, speed=180):
+    """
+    Movement in millimeters of the first drive (nudger + retard rolls)
+    """
+    d1 = NUDGER_FEED_DIR*nudger_distance
+    d2 = TAKEAWAY_FEED_DIR*takeaway_distance
+    d3 = ELEVATOR_FEED_DIR*elevator_distance
+    execute_command(s, 'f{}'.format(speed))
+    execute_command(s, 'g92x0y0z0')
+    execute_command(s, 'g1{}{}{}{}'.format(NUDGER_AXIS, d1,TAKEAWAY_AXIS, d2))
+    execute_command(s, 'g1{}{}'.format(ELEVATOR_AXIS, d3))
 
+def move_sheet(s):
+    """
+    """
+    move(s, nudger_distance=3, takeaway_distance=3, elevator_distance=0.03, speed=1000)
+    move(s, nudger_distance=0, takeaway_distance=3, speed=2000)
+    
 
-def move_sheet():
-	s = initialize()
-	move(s, 2, 2)
-	move(s, 0, 5, 400)
-	s.close()
-
+def move_sheets(s, sheet_count=10):
+    """
+    """
+    for i in range(sheet_count):
+        print("Feeding sheet #{}".format(i))
+        move_sheet(s)
 
 
 if __name__ == '__main__':
-	
-	move_sheet()
+    s = initialize()
+    move_sheets(s, sheet_count=1)
+    # move_sheet(s)
+    s.close()
+    # move_sheet(s)
+    # move(s, elevator_distance=0.05)
