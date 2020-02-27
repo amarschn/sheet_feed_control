6f5dc8d3c613437f713bf748edf6fe578db4b387
amarschn <andrew.marschner@gmail.com>
2020-02-27 17:38:57 -0600

    Adds working code w/nudger actuation. Ran ~50 sheets

--- control.py
+++ control.py
@@ -14,58 +14,85 @@ NUDGER_FEED_DIR = 1
 TAKEAWAY_FEED_DIR = 1
 ELEVATOR_FEED_DIR = 1
 
+NUDGER_ENGAGE_SIGNAL = '1'
+NUDGER_DISENGAGE_SIGNAL = '0'
 
 def initialize():
     """
     """
-    s = serial.Serial('/dev/cu.usbmodem14201',115200)
+    sar_serial = serial.Serial('/dev/cu.usbmodem14201',115200)
+    nudger_serial = serial.Serial('/dev/cu.usbmodem14101',115200)
     # Wake up grbl
-    s.write("\r\n\r\n")
+    sar_serial.write("\r\n\r\n")
     time.sleep(2)   # Wait for grbl to initialize
-    execute_command(s, 'g92x0y0z0')
-    return s
+    execute_sar_command(sar_serial, 'g91')
+    return sar_serial, nudger_serial
 
 
-def execute_command(s, command):
+def execute_sar_command(s, command):
     """
     """
     s.flushInput()  # Flush startup text in serial input
     s.write('{}\n'.format(command.strip()))
     grbl_out = s.readline()
     print 'grbl : ' + grbl_out.strip()
+    # print("HI")
 
+def engage_nudger(n):
+    n.write(NUDGER_ENGAGE_SIGNAL)
+    n.flushInput()
 
-def move(s, nudger_distance=0, takeaway_distance=0, elevator_distance=0, speed=180):
+def disengage_nudger(n):
+    n.write(NUDGER_DISENGAGE_SIGNAL)
+    n.flushInput()
+
+def move_sheet(s, n, nudger_distance=1, feed_distance=1.5, takeaway_distance=4, elevator_distance=0.07, speed=200):
     """
     Movement in millimeters of the first drive (nudger + retard rolls)
     """
-    d1 = NUDGER_FEED_DIR*nudger_distance
-    d2 = TAKEAWAY_FEED_DIR*takeaway_distance
-    d3 = ELEVATOR_FEED_DIR*elevator_distance
-    execute_command(s, 'f{}'.format(speed))
-    execute_command(s, 'g92x0y0z0')
-    execute_command(s, 'g1{}{}{}{}'.format(NUDGER_AXIS, d1,TAKEAWAY_AXIS, d2))
-    execute_command(s, 'g1{}{}'.format(ELEVATOR_AXIS, d3))
+    nudge = NUDGER_FEED_DIR*nudger_distance
+    feed = NUDGER_FEED_DIR*feed_distance
+    takeaway = TAKEAWAY_FEED_DIR*takeaway_distance
+    elevator = ELEVATOR_FEED_DIR*elevator_distance
+    execute_sar_command(s, 'f{}'.format(speed))
+    print("Nudge Operation")
+    engage_nudger(n)
+    execute_sar_command(s, 'g1{}{}{}{}'.format(NUDGER_AXIS, nudge,TAKEAWAY_AXIS, nudge))
+    time.sleep(1)
+    print("Feed + Takeaway Operation")
+    disengage_nudger(n)
+    execute_sar_command(s, 'g1{}{}{}{}'.format(NUDGER_AXIS, feed, TAKEAWAY_AXIS, feed))
+    execute_sar_command(s, 'g1{}{}'.format(TAKEAWAY_AXIS, takeaway))
+    execute_sar_command(s, 'g1{}{}'.format(ELEVATOR_AXIS, elevator))
+    time.sleep(2)
 
-def move_sheet(s):
-    """
-    """
-    move(s, nudger_distance=3, takeaway_distance=3, elevator_distance=0.03, speed=1000)
-    move(s, nudger_distance=0, takeaway_distance=3, speed=2000)
+# def move_sheet(s):
+#     """
+#     """
+#     move(s, nudger_distance=3, takeaway_distance=3, elevator_distance=0.03, speed=1000)
+    # move(s, nudger_distance=0, takeaway_distance=3, speed=2000)
     
 
-def move_sheets(s, sheet_count=10):
+def move_sheets(s, n, sheet_count=10):
     """
     """
     for i in range(sheet_count):
         print("Feeding sheet #{}".format(i))
-        move_sheet(s)
+        move_sheet(s, n)
+
+def test_nudger(n):
+    for i in range(10):
+        engage_nudger(n)
+        time.sleep(0.1)
+        disengage_nudger(n)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    s = initialize()
-    move_sheets(s, sheet_count=1)
-    # move_sheet(s)
+    s, n = initialize()
+    move_sheets(s, n, sheet_count=10)
+    # execute_sar_command(s, 'g1{}{}'.format(NUDGER_AXIS, 10))
+    # test_nudger(n)
+    
     s.close()
-    # move_sheet(s)
-    # move(s, elevator_distance=0.05)
+    n.close()
